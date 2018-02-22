cb42032df4a26ff9e6fb8d0215a0e1c9751bc9ad
Brian Lee <brianlee@Brians-MacBook-Air.local>
2018-02-21 19:24:46 -0800

    added Collage.java

---
+++ src/Collage.java
@@ -0,0 +1,17 @@
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+
+public class Collage {
+	private String[] images;
+	private int[] angles;
+	private BufferedImage collage;
+	private Point2D[] positions;
+	
+	public Collage() {
+		images = new String[30];
+		angles = new int[30];
+		positions = new Point2D[30];
+	}
+	
+	
+}
