755c7d17ca97a51acfc767679461af9b5c3c3e0b
annedao <annedao@usc.edu>
2018-03-07 23:11:12 -0800

    collage unit tests

--- src/collage/CollageJUnitTest.java
+++ src/collage/CollageJUnitTest.java
@@ -1,6 +1,10 @@
 package collage;
 
 import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.List;
 
@@ -14,6 +18,7 @@ import collage.Collage;
 import collage.Picture;
 
 public class CollageJUnitTest {
+	
 	//test for getUrls()
 	@Test
 	public void testGetCatUrls() {
@@ -36,5 +41,32 @@ public class CollageJUnitTest {
 		Collage collage = new Collage();
 		List<String> urls = collage.getUrls("happy dog");
 		assertTrue(urls.size() == 30);
+	} 
+	
+	//test for getAngles
+	@Test
+	// tests getAngles returns 30 angles
+	public void testGetAnglesSize() {
+		Collage collage = new Collage();
+		List<Integer> testAngles = collage.getAngles();
+		assertTrue(testAngles.size() == 30);
+		
+
+	}
+	
+	//test for getAngles
+	@Test
+	// gets getAngles returns random angles between -45 and 45
+	public void testGetAnglesRange() {
+		Collage collage = new Collage();
+		List<Integer> testAngles = collage.getAngles();
+		boolean inRange = true;
+		for (int i = 0; i < testAngles.size(); i++) {
+			if (testAngles.get(i) < -45 || testAngles.get(i) > 45) {
+				inRange = false;
+			}
+		}
+		
+		assertTrue(inRange);
 	}
 }
