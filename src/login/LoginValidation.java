42f9d518be445c5f0c7712d5ef7f7cc94c370f64
MatthewGlein <glein@usc.edu>
2018-04-22 15:25:10 -0700

    Refactoring 4/22

--- src/login/LoginValidation.java
+++ src/login/LoginValidation.java
@@ -36,6 +36,10 @@ public class LoginValidation extends HttpServlet {
 		DataContainer dc = new DataContainer();
 		boolean valid = dc.searchForUser(username, password);
 		
+		//In the above line, the data container is searched to see if the username and password match a login already 
+		// contained within the database. If the combination of user/password does not exsist, valid becomes false,
+		// and the user is not allowed to login, so the page is redirected to the initial login page. If the combination
+		// exists within the data container the user is redirected to the collage build page, which is InputServlet2
 		
 		if(valid) {
 			dc.populateUserClass(username);
